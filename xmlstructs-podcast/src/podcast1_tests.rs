572372a72d526ed97365f96fa6cff66c7edc2ec8
Richard Stephens <rjst01@gmail.com>
2025-11-09 16:15:24 +0100

    test: validate changing namespace-prefix doesn't break document

--- xmlstructs-podcast/src/podcast1_tests.rs
+++ xmlstructs-podcast/src/podcast1_tests.rs
@@ -36,3 +36,20 @@ fn parse_and_check_important_values() {
     let item_8_guid = item_8.guid_elems.get(0).unwrap().value.as_deref();
     assert_eq!(Some("EABDA7EE-1AC6-4B60-9E11-6B3F30B72F87"), item_8_guid);
 }
+
+#[test]
+fn parse_re_namespaced() {
+    let podcast1_rens_xml = include_bytes!("podcast1.re-ns.xml");
+    let podcast1_rens_xml_cursor = Cursor::new(podcast1_rens_xml);
+    let podcast1_rens = RssDocument::parse_document(podcast1_rens_xml_cursor).unwrap();
+
+    // grab the unaltered version for comparison
+    let podcast1_xml = include_bytes!("podcast1.xml");
+    let podcast1_xml_cursor = Cursor::new(podcast1_xml);
+    let podcast1 = RssDocument::parse_document(podcast1_xml_cursor).unwrap();
+
+    // assert the first two episodes are equal
+    let expected_item0 = &podcast1.channel_elems[0].item_elems[0];
+    let actual_item0 = &podcast1_rens.channel_elems[0].item_elems[0];
+    assert_eq!(expected_item0, actual_item0);
+}
